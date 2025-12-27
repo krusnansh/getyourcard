c459d154e3ae257cd7adf993a3f74adcc0afed55
krusnansh <96832443+krusnansh@users.noreply.github.com>
2025-12-27 22:48:01 +0530

    eat: initialize Next.js app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Get Your Card
+      </h1>
+    </main>
+  );
+}
